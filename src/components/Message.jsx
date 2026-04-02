409a358fd4fcc9d10f476e492ae97b371ac6f2cb
Galileo Kim <galileokim451@gmail.com>
2026-04-01 17:16:59 -0700

    fixed up panel

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -13,14 +13,14 @@ export function MessageBasic({
   return (
     <Message className={role === "user" ? "justify-end" : "justify-start"}>
       {isAssistant ? (
-        <div className="w-fit max-w-[95%] sm:max-w-[90%] lg:max-w-[88%]">
-          <div className="bg-secondary p-2 rounded-lg text-foreground text-sm wrap-break-word prose prose-sm">
+        <div className="w-full">
+          <div className="bg-transparent py-2 rounded-lg text-foreground text-sm wrap-break-word prose prose-sm">
             <Markdown>{content}</Markdown>
           </div>
         </div>
       ) : (
-        <div className="w-fit max-w-[80%] sm:max-w-[70%] lg:max-w-[60%]">
-          <MessageContent className="bg-primary text-primary-foreground text-sm wrap-break-word whitespace-pre-wrap">
+        <div className="w-fit max-w-[95%]">
+          <MessageContent className="bg-neutral-100 px-3 text-black text-sm wrap-break-word whitespace-pre-0-wrap">
             {content}
           </MessageContent>
         </div>
